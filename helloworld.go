e4f94669004182a0f0ad3e2287bfb67b09784f54
Alexander <github@shilli.ru>
2015-04-14 17:20:22 +0500

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+        "fmt"
+        "io/ioutil"
+        "net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World from Go in minimal Docker container")
+        fmt.Fprintln(w, "JBOX is online now")
+        files, _ := ioutil.ReadDir("/java")
+            for _, f := range files {
+                    fmt.Fprintln(w, f.Name())
+            }
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+        http.HandleFunc("/", helloHandler)
 
-	fmt.Println("Started, serving at 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
+        fmt.Println("Started, serving at 80")
+        err := http.ListenAndServe(":80", nil)
+        if err != nil {
+                panic("ListenAndServe: " + err.Error())
+        }
 }
